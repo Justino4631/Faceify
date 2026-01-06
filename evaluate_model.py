689851a439abc9b3b4bf0ce927bd082c92d38352
Justin <Justin.M.Baratta@outlook.com>
2026-01-05 19:42:16 -0800

    Added an evaluation file that uses saved model.pth and predicts what face is in the new image

---
+++ evaluate_model.py
@@ -0,0 +1,34 @@
+from PIL import Image
+import torchvision.transforms as transforms
+import torch
+from torch import nn
+from model import Model
+from model import get_classes
+import numpy as np
+from image_filters import crop_image, convert_to_grayscale
+classes = get_classes()
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+model = Model().to(device)
+model.load_state_dict(torch.load("model.pth", map_location=device))
+model.eval()
+
+transform = transforms.Compose([
+    transforms.Grayscale(num_output_channels=1),
+    transforms.Resize((128, 128)),
+    transforms.ToTensor()
+])
+
+img = Image.open(input("Enter filepath and filename (ex. evaluate_images/AdamSandler.jpg): "))
+rgb_array = np.asarray(img)
+grayscale_cropped = crop_image(convert_to_grayscale(rgb_array))
+
+tensor = torch.from_numpy(grayscale_cropped).float().unsqueeze(0).unsqueeze(0).to(device)
+
+with torch.no_grad():
+    output = model(tensor)
+    print(output)
+    pred = torch.argmax(output, dim=1)
+    pred = classes[pred.item()]
+
+print(pred)
